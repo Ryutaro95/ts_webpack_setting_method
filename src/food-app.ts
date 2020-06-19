6d4089f320697fb9f1c9423d9c5f13f069d171eb
Ryutaro95 <ryutaro9595@gmail.com>
2020-06-19 10:18:28 +0900

    クリックされた要素を配列に格納する処理を追加

--- src/food-app.ts
+++ src/food-app.ts
@@ -7,8 +7,19 @@ class Food {
     this.element.classList.toggle('food--active');
   }
 }
+
 class Foods {
   elements = document.querySelectorAll<HTMLDivElement>('.food');
+  private _activeElements: HTMLDivElement[] = [];
+  get activeElements() {
+    this._activeElements = [];
+    this.elements.forEach(element => {
+      if (element.classList.contains('food--active')) {
+        this._activeElements.push(element);
+      }
+    })
+    return this._activeElements;
+  }
   constructor() {
     this.elements.forEach(element => {
       new Food(element);
